13fab0908273df97d3a54e552653bc95826a71f2
Kieran Rye <ryki16@nti.umea>
2017-09-11 10:17:34 +0200

    first commit

---
+++ Lektion1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Lektion1{
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Wattup " +
+			Toolkit.getDefaultToolkit() .beep())	;{
+				System.getProperty("user.name");
+			}
+	}
+}
