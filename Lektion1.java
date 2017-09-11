59e0000f3af84fe2af408ee239f25f4783e83a5f
Kieran Rye <ryki16@nti.umea>
2017-09-11 11:00:20 +0200

    added all practice questions and questions

--- Lektion1.java
+++ Lektion1.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class Lektion1{
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Wattup " +
-			Toolkit.getDefaultToolkit() .beep())	;{
-				System.getProperty("user.name");
-			}
+		//JOptionPane.showMessageDialog(null, "Wattup " +
+			//Toolkit.getDefaultToolkit() .beep())	;{
+				//System.getProperty("user.name");
+		//JOptionPane.showMessageDialog(null, "Datum och tid \n" + Calendar.getInstance () .getTime() .toString());
+		JOptionPane.showMessageDialog(null, "qestion", "Question", JOptionPane.QUESTION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "info",  "Infomation", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "warning", "Warning", JOptionPane.WARNING_MESSAGE);
+		JOptionPane.showMessageDialog(null, "error", "Error", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "plain", "Plain", JOptionPane.PLAIN_MESSAGE);
+
 	}
 }
